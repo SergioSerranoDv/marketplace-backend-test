7ec420ad2db503c789ac45b62ea62b5f2674ee14
Sergio Serrano <sergioandresgmd10@gmail.com>
2024-03-18 18:47:21 -0500

    Fix CORS origin configuration

--- src/App.ts
+++ src/App.ts
@@ -10,7 +10,7 @@ export class App {
       App.instance.use(express.urlencoded({ extended: true }))
       App.instance.use(
         cors({
-          // origin: "*",
+          origin: "*",
           // methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
           // allowedHeaders: "Content-Type,Authorization",
         })
